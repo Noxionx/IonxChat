dc63f857c8b1fb8409c8cbdfb8a21f1ab9f82c39
Florent GADE <florent.gade@mediego.com>
2015-04-17 17:43:42 +0200

    Added sqlite users DB for session on real, registered users

--- ressources/users.js
+++ ressources/users.js
@@ -1,3 +1,8 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('userDB')
+var crypto = require('crypto');
+
+
 var default_user = {
 	'username': 'default_user',
 	'password': '12345'
@@ -5,20 +10,40 @@ var default_user = {
 
 var findOne = function(params, callback){
 	if(params && params.username){
-		if(default_user.username == params.username){
-			var user = {}
-			user.__proto__ = default_user
-			user.validPassword = function(pwd){
-				return this.password == pwd
+		db.get('SELECT * FROM users WHERE username=?', [params.username], function(err, row){
+			if(err){
+				console.log(err)
+				!!callback && callback(null)
 			}
-			!!callback && callback(null, user)
-		}
-		else{
-			!!callback && callback(null)
-		}
+			else{
+				if(!!row && row.username == params.username){
+					var user = {}
+					user.__proto__ = row
+					user.validPassword = function(pwd){
+						return this.password == crypto.createHash('sha1').update(pwd).digest('hex')
+					}
+					!!callback && callback(null, user)
+				}
+				else{
+					!!callback && callback(null)
+				}
+			}
+		})
 	}
 	else{
 		!!callback && callback("Bad parameters")
 	}
+		
 }
 exports.findOne = findOne
+
+var initUserTable = function(callback){
+	db.exec("DROP TABLE IF EXISTS users")
+	.exec("CREATE TABLE IF NOT EXISTS users (username Varchar(255), password Varchar(255))")
+	.exec("INSERT INTO users (username, password) VALUES ('noxionx', '"+crypto.createHash('sha1').update('warcraft3').digest('hex')+"')")
+	.exec("INSERT INTO users (username, password) VALUES ('spacexion', '"+crypto.createHash('sha1').update('azerty13011992').digest('hex')+"')")
+	.exec("INSERT INTO users (username, password) VALUES ('lixoteamionx', '"+crypto.createHash('sha1').update('pikachu35400').digest('hex')+"')")
+}
+exports.initUserTable = initUserTable
+
+initUserTable()
--- server.js
+++ server.js
@@ -6,13 +6,18 @@ require("./authentication.js")(app)
 app.use(express.static('web'));
 
 app.get('/', function (req, res) {
-  if(req.user){
-  	res.send("hello")
+  if(req.user!==undefined){
+  	console.log("User : ")
+  	res.sendFile('index.html')
   }
   else{
   	res.redirect("/login")
   }
 });
+
+// app.get('/currentuser', function(req, res){
+// 	res.json({'user': req.user || null})
+// })
 
 var server = app.listen(3000, function () {
 
